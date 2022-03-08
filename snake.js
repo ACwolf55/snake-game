1e3050a390b4a02dfadd0190eefc312d20006ac2
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2022-03-07 23:27:08 -0700

    started game run loop, made game board, started snake body

---
+++ snake.js
@@ -0,0 +1,13 @@
+export const SNAKE_SPEED = 2
+const snakeBody = [{x:11,y:11}]
+
+export function updateSnake (){
+    console.log('update snake')
+}
+
+export function drawSnake (){
+    console.log('draw snake')
+    snakeBody.forEach(segment=>{
+    // -----------------------------------------------------    CODE LEFT OFF HERE     / CODE BOOK MARK   !!!!  ---------------
+    })
+}
